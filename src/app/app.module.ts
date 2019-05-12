ca361f58326d69d6d5ec36bacf019042759c502a
Mateusz Kłosiński <mkklosin@gmail.com>
2019-05-12 10:43:00 +0200

    Material init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthPageModule } from './pages/auth-page/auth-page.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material/material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthPageModule],
+  imports: [BrowserModule, AppRoutingModule, AuthPageModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
