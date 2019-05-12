16d584627f89a1afa87e7df6c5f48f9edfdc53a1
Mateusz Kłosiński <mkklosin@gmail.com>
2019-05-12 10:22:58 +0200

    Prettier init with auth pages

---
+++ src/app/pages/auth-page/auth-page.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthPageComponent } from './auth-page.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [AuthPageComponent, SignInComponent, SignUpComponent],
+  imports: [CommonModule]
+})
+export class AuthPageModule {}
