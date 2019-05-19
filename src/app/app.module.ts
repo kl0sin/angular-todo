d417127a5411acbafe8834008be95aa2244dec51
Mateusz Kłosiński <mkklosin@gmail.com>
2019-05-19 23:19:55 +0200

    Lint fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListPageModule } from './pages/list-page/list-page.module';
 import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule, ListPageModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, BrowserAnimationsModule],
-  exports: [BrowserAnimationsModule]
+  imports: [CommonModule, BrowserAnimationsModule, MatButtonModule],
+  exports: [BrowserAnimationsModule, MatButtonModule]
 })
 export class MaterialModule {}
